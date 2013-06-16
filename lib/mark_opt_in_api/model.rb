574fc6488c33b821c8bc7aaf56926a1141544101
Vitalya Shevtsov <kaktusyaka@gmail.com>
2013-06-16 17:08:34 +0300

    added rspec tests

--- lib/mark_opt_in_api/model.rb
+++ lib/mark_opt_in_api/model.rb
@@ -1,12 +1,14 @@
 class Marketing < ActiveRecord::Base
 
+  attr_accessible :mobile, :first_name, :last_name, :company_name, :email, :channel, :permission_type
+
   AVAILABLE_PERMISSION_TYPES = %w(one-time permanent)
   AVAILABLE_CHANNEL_TYPES     = %w(sms email sms+email)
 
   validates :mobile, presence: true, length: { maximum: 255 }, format: { with: /\A\+?[0-9\-\(\) ]*\Z/ }
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
-  validates :last_name, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
   validates :company_name, presence: true, uniqueness: true, length: { maximum: 255 }
   validates :email, email_format: true, uniqueness: true, length: { maximum: 255 }
   validates :channel, inclusion: { in: AVAILABLE_CHANNEL_TYPES }
--- spec/marketings_spec.rb
+++ spec/marketings_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 describe Marketing do
   context 'Validations' do
-    it { should validate_email_format_of(:first_name) }
-  end
+    it { should validate_presence_of(:first_name) }
+    it { should ensure_length_of(:first_name).is_at_most(255) }
+    it { should validate_presence_of(:last_name) }
+    it { should ensure_length_of(:last_name).is_at_most(255) }
+    it { should ensure_length_of(:email).is_at_most(255) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:mobile) }
+    it { should ensure_length_of(:mobile).is_at_most(255) }
+    it { should validate_presence_of(:company_name) }
+    it { should validate_uniqueness_of(:company_name) }
+    it { should ensure_length_of(:company_name).is_at_most(255) }
+    it { should ensure_inclusion_of(:channel).in_array(Marketing::AVAILABLE_CHANNEL_TYPES) }
+    it { should ensure_inclusion_of(:permission_type).in_array(Marketing::AVAILABLE_PERMISSION_TYPES) }
 
-  it "new Marketing should be not valid" do
-    Marketing.new.valid?.should be_false
+    it { should allow_value('kaktusyaka@gmail.com', 'apapap.dodod@gmail.com').for(:email) }
+    it { should_not allow_value('', 'apapap.gmail.com').for(:email) }
   end
 end
