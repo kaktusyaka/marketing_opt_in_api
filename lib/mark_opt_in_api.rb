82b6c5c2858f2889a81dbd08ecbe22d49b603e9f
Vitalya Shevtsov <kaktusyaka@gmail.com>
2013-06-17 20:13:23 +0300

    change api class name

--- lib/mark_opt_in_api.rb
+++ lib/mark_opt_in_api.rb
@@ -1,7 +1,7 @@
 require "mark_opt_in_api/version"
 
 module MarkOptInApi
-  class MarkOptInApi
+  class Api
     def add
       response = HTTParty.post('http://localhost:4567/marketings')
       puts response.body, response.code, response.message, response.headers.inspect
